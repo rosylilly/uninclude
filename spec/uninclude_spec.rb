fe935046bffc1f7f63ed54f4fd3fc31f7c9575c2
Sho Kusano <rosylilly@aduca.org>
2013-03-18 03:18:23 +0900

    Write spec

--- spec/uninclude_spec.rb
+++ spec/uninclude_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
+module ExampleMod
+  def mod; :mod; end
+end
+
 describe Uninclude do
-  it 'should have a version number' do
-    Uninclude::VERSION.should_not be_nil
+  let(:klass) {
+    Class.new do
+    end
+  }
+  let(:instance) { klass.new }
+
+  describe '.uninclude' do
+    it 'should uninclude module' do
+      klass.class_eval { include ExampleMod }
+      instance.should respond_to(:mod)
+      klass.class_eval { uninclude ExampleMod }
+      instance.should_not respond_to(:mod)
+    end
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe '#unextend' do
+    it 'should unextend module' do
+      instance.extend(ExampleMod)
+      instance.should respond_to(:mod)
+      instance.unextend(ExampleMod)
+      instance.should_not respond_to(:mod)
+    end
   end
 end
